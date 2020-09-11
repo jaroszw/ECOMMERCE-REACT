3c98254ba7c92ee982c0f3e83eaaa58dc3a0a895
Wojciech Jarosz <jaroszw@gmail.com>
2020-09-11 12:02:55 +0200

    Added firebase utilities - sign-up with email and stroing it in firebase DB

--- src/App.js
+++ src/App.js
@@ -30,8 +30,9 @@ class App extends React.Component {
               id: snapShot.id, 
               ...snapShot.data()
             }
-          }, () => {console.log(this.state)
           })
+
+          console.log(this.state)
         });
       } else {
         this.setState({currentUser: userAuth})
