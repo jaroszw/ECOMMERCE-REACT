225facf3d66cb87220f9881f4fd8b5ad5758beaa
Wojciech Jarosz <jaroszw@gmail.com>
2020-10-19 19:20:31 +0200

    advanced routing, shop data and preview data into reducers

--- src/components/collections.overview/collections-overview.component.jsx
+++ src/components/collections.overview/collections-overview.component.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import './collections-overview.style.scss'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import './collections-overview.style.scss';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selector'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 import { createStructuredSelector } from 'reselect';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
-const CollectionOverview = ({ collections }) => (
-    <div className='collections-overveiew'>
-        {collections.map(({ id, ...otherCollectionsProps }) => (
-            <CollectionPreview key={id} {...otherCollectionsProps} />
-        ))
-        }
+const CollectionOverview = ({ collections }) => {
+  console.log(collections);
+
+  return (
+    <div className="collections-overveiew">
+      {collections.map(({ id, ...otherCollectionsProps }) => (
+        <CollectionPreview key={id} {...otherCollectionsProps} />
+      ))}
     </div>
-)
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
+  collections: selectCollectionsForPreview,
+});
 
 export default connect(mapStateToProps)(CollectionOverview);
