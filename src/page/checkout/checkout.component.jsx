e224015eddbcccc32f65435feafbb5fa98e2024d
Wojciech Jarosz <jaroszw@gmail.com>
2020-10-20 11:49:38 +0200

    udpating stripe integration - updatinv favicon and tile of the page

--- src/page/checkout/checkout.component.jsx
+++ src/page/checkout/checkout.component.jsx
@@ -8,6 +8,8 @@ import {
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import './checkout.styles.scss';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
+
 const CheckoutPage = ({ cartItems, total }) => {
   return (
     <div className="checkout-page">
@@ -32,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">TOTAL: ${total} </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
