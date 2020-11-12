7e459f92b8053e87bb87c7593c3a285ee08a9269
Wojciech Jarosz <jaroszw@gmail.com>
2020-11-12 21:51:57 +0100

    Root Saga implementing - after take, takeEvery, takeLatest saga examples

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './root-reducer';
-import { fetchCollectionsStart } from './shop/shop.saga';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
